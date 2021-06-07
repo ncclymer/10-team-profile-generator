36499ffe4f2b6971ab7e85d47dd67c4d9afec17b
unknown <nick_clymer2018@yahoo.com>
2021-06-07 10:58:27 -0600

    added dependencies and installation json files

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const manager = require("./lib/manager");
+const test = require('jest');
