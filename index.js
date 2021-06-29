2d68462a3f1baed79e60c45c9633ed07d340cde2
unknown <nick_clymer2018@yahoo.com>
2021-06-28 19:46:51 -0600

    refactor in progress. Streamlined inquier

--- index.js
+++ index.js
@@ -1,202 +1,259 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const engineer = require("./lib/engineer");
-const intern = require("./lib/intern");
-const manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
 
 const employee = [];
 
-function initIndex() {
-    initHtml();
-    addEmp();
+const generateTeam = [{
+    type: "input",
+    name: "managerName",
+    message: "What is the Manager's name?",
+ 
+},
+{
+    type: "input",
+    name: "managerId",
+    message: "What is the Manager's employee ID?",
+
+},
+{
+    type: "input",
+    name: "managerEmail",
+    message: "What is the Manager's e-mail?",
+
+},
+{
+    type: "input",
+    name: "managerPhone",
+    message: "What is the Manager's phone number?",
+
+},
+];
+
+const addNewEmployee = [{
+type: "list",
+name: "empAdd",
+message: "Would you like to add another employee?",
+choices: ["Yes", "No"],
+}, ];
+
+const employeeType = [{
+type: "list",
+name: "typeEmp",
+message: "Would you like to add an employee to your team?",
+choices: ["Engineer", "Intern"],
+}, ];
+
+const questionEngineer = [{
+    type: "input",
+    name: "engName",
+    message: "What is the Engineer's name?",
+},
+{
+    type: "input",
+    name: "engId",
+    message: "What is the Engineer's ID Number?",
+},
+{
+    type: "input",
+    name: "engEmail",
+    message: "What is the Engineer's e-mail?",
+},
+{
+    type: "input",
+    name: "engGithub",
+    message: "What is the Engineer's GitHub account?",
+},
+];
+const questionIntern = [{
+    type: "input",
+    name: "intName",
+    message: "What is the Intern's name?",
+},
+{
+    type: "input",
+    name: "intId",
+    message: "What is the Intern's ID Number?",
+},
+{
+    type: "input",
+    name: "intEmail",
+    message: "What is the Intern's e-mail?",
+},
+{
+    type: "input",
+    name: "intSchool",
+    message: "What school does the Intern go to?",
+},
+];
+
+function writeToFile(fileName, f) {
+fs.writeFileSync(fileName, f, (error) => {
+    error ? console.log(err) : console.log("Your team's profile is ready to view!");
+});
 }
 
-function addEmp() {
-    inquirer.prompt([{
-        type: "input",
-        message: "Please enter the manager's name.",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please enter the manger's id.",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Please enter the manager's email address.",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Please enter the manager's office phone number.",
-        name: "phone"
-    },
-    {
-        type: "list",
-        message: "Would you like to add an:",
-        choices: [
-            "intern",
-            "engineer",
-            "Done building team."
-        ],
-        name: "role"
+function newIntern() {
+inquirer.prompt(questionIntern).then((res) => {
+    employee.push(new Intern(res.intName, res.intId, res.intEmail, res.intSchool));
+    addEmployees();
+});
+}
+
+function newEngineer() {
+inquirer.prompt(questionEngineer).then((res) => {
+    employee.push(
+        new Engineer(res.engName, res.engId, res.engEmail, res.engGithub)
+    );
+    addEmployees();
+});
+}
+
+function newEmployee() {
+inquirer.prompt(employeeType).then((res) => {
+    console.log(res);
+    res.typeEmp === "Engineer" ? newEngineer() : newIntern();
+});
+}
+
+function addEmployees() {
+inquirer.prompt(addNewEmployee).then((res) => {
+    res.empAdd === "Yes" ? newEmployee() : writeToFile("./home/roster.html", teamProfile(employee));
+});
+}
+
+function init() {
+            inquirer.prompt(generateTeam).then((res) => {
+                employee.push(
+                    new Manager(
+                        res.managerName,
+                        res.managerId,
+                        res.managerEmail,
+                        res.managerPhone
+                    )
+                );
+                addEmployees();
+            }
+            );
+        }
+init();
+
+function genEmployees(employee) {
+let htmlMain = "";
+employee.forEach((obj) => {
+    switch (obj.getRole()) {
+        case "Engineer":
+            let stringDataEng = genEngineer(obj);
+            htmlMain += stringDataEng;
+            break;
+        case "Intern":
+            let stringDataInt = genIntern(obj);
+            htmlMain += stringDataInt;
+            break;
+        default:
+            return "";
     }
-    ])
-        .then(function ({ name, role, id, email }) {
-            switch (role) {
-                case 'intern':
-                    inquirer.prompt({
-                        type: 'input',
-                        message: "what is your school name?",
-                        name: 'school'
-                    })
-                    .then(function ({ school }) {
-                        additionalEmp(name, role, id, email, school)
-                    })
-                    break
-                case 'engineer':
-                    inquirer.prompt({
-                        type: 'input',
-                        message: "what is your github user name?",
-                        name: 'github'
-                    }).then(function ({ github }) {
-                        addEngineer(name, role, id, email, github)
-                    })
-                    break
-            }
-
-            let roleData = "";
-            if (role === "intern") {
-                roleData = "school name";
-            } else if (role === "engineer") {
-                roleData = "github";
-            } else {
-                roleData = "phone";
-            }
-            inquirer.prompt([{
-
-                type: "list",
-                message: "Are there additional employees to add?",
-                choices: [
-                    "yes",
-                    "no"
-                ],
-                name: "additional"
-            }])
-                .then(function ({ roleData, additional }) {
-                    let additionalEmp;
-                    if (role === "intern") {
-                        additionalEmp = new intern(name, id, email, roleData);
-                    } else if (role === "engineer") {
-                        additionalEmp = new engineer(name, id, email, roleData);
-                    } else {
-                        additionalEmp = new manager(name, id, email, roleData);
-                    }
-                    employee.push(additionalEmp);
-                    newHtml(additionalEmp)
-                        .then(function () {
-                            if (additional === "yes") {
-                                addEmp();
-                            } else {
-                                endHtml();
-                            }
-                        });
-                });
-        });
+});
+return htmlMain;
 }
 
-
-function initHtml() {
-    const html = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            <title>My Team</title>
-        </head>
-        <body>
-            <nav class="navbar navbar-dark bg-dark mb-5">
-                <span class="navbar-brand mb-0 h1 w-100 text-center">My Team</span>
-            </nav>
-            <div class="container">
-                <div class="row">`;
-    fs.writeFile("./home/roster.html", html, function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    console.log("Starting employee registration.");
-}
-
-function newHtml(member) {
-    return new Promise(function (resolve, reject) {
-        const name = member.getName();
-        const role = member.getRole();
-        const id = member.getId();
-        const email = member.getEmail();
-        let data = "";
-        if (role === "intern") {
-            const school = member.getSchool();
-            data = `<div class="col-6">
-                <div class="card mx-auto mb-3" style="width: 18rem">
-                <h5 class="card-header">${name}<br /><br />Intern</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${id}</li>
-                    <li class="list-group-item">Email Address: <a href="mailto:${email}">${email}</a></li>
-                    <li class="list-group-item">School: ${school}</li>
-                </ul>
-                </div>
-            </div>`;
-        } else if (role === "engineer") {
-            const gitHub = member.getGitHub();
-            data = `<div class="col-6">
-                <div class="card mx-auto mb-3" style="width: 18rem">
-                <h5 class="card-header">${name}<br /><br />Engineer</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${id}</li>
-                    <li class="list-group-item">Email Address: <a href="mailto:${email}">${email}</a></li>
-                    <li class="list-group-item">GitHub: <a href="https://github.com/${gitHub}" target="_blank">${gitHub}</a></li>
-                </ul>
-                </div>
-            </div>`;
-        } else {
-            const officePhone = member.getOfficeNum();
-            data = `<div class="col-6">
-                <div class="card mx-auto mb-3" style="width: 18rem">
-                <h5 class="card-header">${name}<br /><br />Manager</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${id}</li>
-                    <li class="list-group-item">Email Address: <a href="mailto:${email}">${email}</a></li>
-                    <li class="list-group-item">Office Phone: ${officePhone}</li>
-                </ul>
-                </div>
-            </div>`
-        }
-        console.log("Adding team member(s).");
-        fs.appendFile("./home/roster.html", data, function (err) {
-            if (err) {
-                return reject(err);
-            };
-            return resolve();
-        });
-    });
-}
-
-function endHtml() {
-    const html = ` </div>
+function genManager(employee) {
+let managerData = "";
+employee.forEach((obj) => {
+    if (obj.getRole() === "Manager") {
+        managerData = `			
+    <div class="row center-align">
+    <div class="col s12 m4 offset-m4">
+        <div class="card blue center-align">
+            <div class="card-content black-text">
+                <span class="card-title">${obj.getName()}</span>
+                <p>${obj.getRole()}</p>
+            </div>
+            <div class="card-action">
+                <a href="#">${obj.getId()}</a>
+                <a href="#">${obj.getEmail()}</a>
+                <a href="#">${obj.getOfficeNumber()}</a>
+            </div>
         </div>
-        </body>
-        </html>`;
-
-    fs.appendFile("./home/roster.html", html, function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    console.log("Completed employee registration.");
+    </div>
+    </div>
+        `;
+    } else {
+        return "";
+    }
+});
+return managerData;
 }
 
-initIndex();
+function genEngineer(obj) {
+return `			
+<div class="row center-align">
+<div class="col s12 m4 offset-m4">
+<div class="card blue center-align">
+  <div class="card-content black-text">
+    <span class="card-title">${obj.getName()}</span>
+    <p>${obj.getRole()}</p>
+  </div>
+  <div class="card-action">
+    <a href="#">${obj.getId()}</a>
+    <a href="#">${obj.getEmail()}</a>
+    <a href="#">${obj.getGitHub()}</a>
+  </div>
+</div>
+</div>
+</div>
+`;
+}
+
+function genIntern(obj) {
+return `			
+<div class="row center-align">
+<div class="col s12 m4 offset-m4">
+<div class="card blue center-align">
+  <div class="card-content black-text">
+    <span class="card-title">${obj.getName()}</span>
+    <p>${obj.getRole()}</p>
+  </div>
+  <div class="card-action">
+    <a href="#">${obj.getId()}</a>
+    <a href="#">${obj.getEmail()}</a>
+    <a href="#">${obj.getSchool()}</a>
+  </div>
+</div>
+</div>
+</div>
+`;
+}
+
+function teamProfile(employee) {
+return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<title>Team</title>
+</head>
+<body>
+<nav>
+<div class="nav-wrapper blue-grey lighten-2">
+<a class="brand-logo center black-text">My Team</a>
+<ul id="nav-mobile" class="left hide-on-med-and-down">
+</ul>
+</div>
+</nav>
+<div class="row center-align">
+${genManager(employee)}
+</div>
+<div class="row center-align">
+${genEmployees(employee)}
+</div>
+</body>
+</html>
+`;
+}
